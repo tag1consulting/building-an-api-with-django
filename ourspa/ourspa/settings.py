af1d42ffaf76fccfed01efecb18c7c37febe0067
Jeremy Andrews <jeremy@amailbox.net>
2018-01-07 16:29:21 +0100

    Enable spauser app and user model

--- ourspa/ourspa/settings.py
+++ ourspa/ourspa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'spauser',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'spauser.SpaUser'
