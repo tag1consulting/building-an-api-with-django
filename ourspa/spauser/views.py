d542d08b3a10b5fd9fd22640139147ac34ccafc8
Jeremy Andrews <jeremy@amailbox.net>
2018-01-07 17:21:51 +0100

    add SpaUserLogoutAllView class

--- ourspa/spauser/views.py
+++ ourspa/spauser/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import uuid
+from rest_framework import views, permissions, status
+from rest_framework.response import Response
 
-# Create your views here.
+class SpaUserLogoutAllView(views.APIView):
+    """
+    Use this endpoint to log out all sessions for a given user.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        user.jwt_secret = uuid.uuid4()
+        user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
