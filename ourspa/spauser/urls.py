6398298924885e1be59bc0d5166a65b62021eb61
Jeremy Andrews <jeremy@amailbox.net>
2018-01-07 17:24:14 +0100

    add route for logging out

--- ourspa/spauser/urls.py
+++ ourspa/spauser/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import re_path
 
 from djoser import views as djoser_views
 from rest_framework_jwt import views as jwt_views
+from spauser import views
 
 
 urlpatterns = [
@@ -9,6 +10,7 @@ urlpatterns = [
     re_path(r'^user/view/$', djoser_views.UserView.as_view(), name='user-view'),
     re_path(r'^user/delete/$', djoser_views.UserDeleteView.as_view(), name='user-delete'),
     re_path(r'^user/create/$', djoser_views.UserCreateView.as_view(), name='user-create'),
+    re_path(r'^user/logout/all/$', views.SpaUserLogoutAllView.as_view(), name='user-logout-all'),
 
     # Views are defined in Rest Framework JWT, but we're assigning custom paths.
     re_path(r'^user/login/$', jwt_views.ObtainJSONWebToken.as_view(), name='user-login'),
