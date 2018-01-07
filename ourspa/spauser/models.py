ae61bedb78b1d54d0a18ba6d1dc56f5324147ef6
Jeremy Andrews <jeremy@amailbox.net>
2018-01-07 16:28:33 +0100

    Add SpaUser custom user model

--- ourspa/spauser/models.py
+++ ourspa/spauser/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class SpaUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and
+        password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class SpaUser(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = SpaUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+            return self.email
