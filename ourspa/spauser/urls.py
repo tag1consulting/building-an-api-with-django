b927839c2a9f19303f927b4b39290fbcea10d864
Jeremy Andrews <jeremy@amailbox.net>
2018-01-08 11:07:58 +0100

    add in paths with new permissions

--- ourspa/spauser/urls.py
+++ ourspa/spauser/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import re_path
-
 from djoser import views as djoser_views
 from rest_framework_jwt import views as jwt_views
 from spauser import views
 
 
 urlpatterns = [
-    # Views are defined in Djoser, but we're assigning custom paths.
-    re_path(r'^user/view/$', djoser_views.UserView.as_view(), name='user-view'),
-    re_path(r'^user/delete/$', djoser_views.UserDeleteView.as_view(), name='user-delete'),
-    re_path(r'^user/create/$', djoser_views.UserCreateView.as_view(), name='user-create'),
+    re_path(r'^user/view/$', views.SpaUserView.as_view(), name='user-view'),
+    re_path(r'^user/delete/$', views.SpaUserDeleteView.as_view(), name='user-delete'),
     re_path(r'^user/logout/all/$', views.SpaUserLogoutAllView.as_view(), name='user-logout-all'),
+
+    # Views are defined in Djoser, but we're assigning custom paths.
+    re_path(r'^user/create/$', djoser_views.UserCreateView.as_view(), name='user-create'),
 
     # Views are defined in Rest Framework JWT, but we're assigning custom paths.
     re_path(r'^user/login/$', jwt_views.ObtainJSONWebToken.as_view(), name='user-login'),
