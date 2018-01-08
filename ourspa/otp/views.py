75d5420e3490047df464026721f826c262805e81
Jeremy Andrews <jeremy@amailbox.net>
2018-01-08 09:32:54 +0100

    add view classes for creating and verifying TOTP devices

--- ourspa/otp/views.py
+++ ourspa/otp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import views, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from django_otp import devices_for_user
+from django_otp.plugins.otp_totp.models import TOTPDevice
 
-# Create your views here.
+
+def get_user_totp_device(self, user, confirmed=None):
+    devices = devices_for_user(user, confirmed=confirmed)
+    for device in devices:
+        if isinstance(device, TOTPDevice):
+            return device
+
+class TOTPCreateView(views.APIView):
+    """
+    Use this endpoint to set up a new TOTP device
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        user = request.user
+        device = get_user_totp_device(self, user)
+        if not device:
+            device = user.totpdevice_set.create(confirmed=False)
+        url = device.config_url
+        return Response(url, status=status.HTTP_201_CREATED)
+
+class TOTPVerifyView(views.APIView):
+    """
+    Use this endpoint to verify/enable a TOTP device
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, token, format=None):
+        user = request.user
+        device = get_user_totp_device(self, user)
+        if not device == None and device.verify_token(token):
+            if not device.confirmed:
+                device.confirmed = True
+                device.save()
+            return Response(True, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
