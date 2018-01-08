76bb83d2e4f9fdcd5ff7ce80d3e7a6f32df58b49
Jeremy Andrews <jeremy@amailbox.net>
2018-01-08 11:06:31 +0100

    add custom permissions to view and delete user

--- ourspa/spauser/views.py
+++ ourspa/spauser/views.py
@@ -1,6 +1,12 @@
 import uuid
+from djoser.views import UserView, UserDeleteView
+from djoser import serializers
 from rest_framework import views, permissions, status
 from rest_framework.response import Response
+from rest_framework import permissions
+from spauser.models import SpaUser
+from otp import permissions as otp_permissions
+
 
 class SpaUserLogoutAllView(views.APIView):
     """
@@ -13,3 +19,20 @@ class SpaUserLogoutAllView(views.APIView):
         user.jwt_secret = uuid.uuid4()
         user.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class SpaUserView(UserView):
+    """
+    Uses the default Djoser view, but add the IsOtpVerified permission.
+    Use this endpoint to retrieve/update user.
+    """
+    model = SpaUser
+    serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.IsAuthenticated, otp_permissions.IsOtpVerified]
+ 
+class SpaUserDeleteView(UserDeleteView):
+    """
+    Uses the default Djoser view, but add the IsOtpVerified permission.
+    Use this endpoint to remove actually authenticated user.
+    """
+    serializer_class = serializers.UserDeleteSerializer
+    permission_classes = [permissions.IsAuthenticated, otp_permissions.IsOtpVerified]
