69e4aad6a31efb4b4183efefd45713dd672d0ab1
Jeremy Andrews <jeremy@amailbox.net>
2018-01-08 12:55:15 +0100

    add routes for creating and verifying static tokens

--- ourspa/otp/urls.py
+++ ourspa/otp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     re_path(r'^totp/create/$', views.TOTPCreateView.as_view(), name='totp-create'),
     re_path(r'^totp/login/(?P<token>[0-9]{6})/$', views.TOTPVerifyView.as_view(), name='totp-login'),
+    re_path(r'^static/create/$', views.StaticCreateView.as_view(), name='static-create'),
+    re_path(r'^static/login/(?P<token>[a-z2-9]{7,8})/$', views.StaticVerifyView.as_view(), name='static-login'),
 ]
