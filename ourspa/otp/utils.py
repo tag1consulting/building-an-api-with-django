e6bc97db958ad190f4c74dbd1ba418becb9e9102
Jeremy Andrews <jeremy@amailbox.net>
2018-01-08 10:33:32 +0100

    add custom jwt_otp_payload handler

---
+++ ourspa/otp/utils.py
@@ -0,0 +1,40 @@
+from calendar import timegm
+from datetime import datetime
+from rest_framework_jwt.compat import get_username, get_username_field
+from rest_framework_jwt.settings import api_settings
+from django_otp.models import Device
+
+
+def jwt_otp_payload(user, device = None):
+    """
+    Optionally include OTP device in JWT payload
+    """
+    username_field = get_username_field()
+    username = get_username(user)
+        
+    payload = {
+        'user_id': user.pk,
+        'username': username,
+        'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA
+    }
+        
+    # Include original issued at time for a brand new token,
+    # to allow token refresh
+    if api_settings.JWT_ALLOW_REFRESH:
+        payload['orig_iat'] = timegm(
+            datetime.utcnow().utctimetuple()
+        )
+
+    if api_settings.JWT_AUDIENCE is not None:
+        payload['aud'] = api_settings.JWT_AUDIENCE
+
+    if api_settings.JWT_ISSUER is not None:
+        payload['iss'] = api_settings.JWT_ISSUER
+
+    # UserAPI additions
+    if (user is not None) and (device is not None) and (device.user_id == user.id) and (device.confirmed is True):
+        payload['otp_device_id'] = device.persistent_id
+    else:
+        payload['otp_device_id'] = None
+
+    return payload
